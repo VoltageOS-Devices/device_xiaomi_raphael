29bc8494078f291e4ed151fa9fdf77c2d104eaf8
Yichi Chen <yichichen@google.com>
2019-08-01 17:50:50 -0700

    raphael: enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to
    HWC queue stuffing
    
    Bug: 138601429
    Test: 1. Open Camera
          2. Take some pictures
          3. Swipe vertically to go back to home screen
          4. Measure the "FrameMissed" count from systrace result
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I1392ae833f246351bdb8398e6e63e2e787f41af2

--- device.mk
+++ device.mk
@@ -199,6 +199,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.early.app.duration=16500000
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.earlyGl.sf.duration=13500000
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.earlyGl.app.duration=21000000
 
+# Enable backpressure for GL comp
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_gl_backpressure=1
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
